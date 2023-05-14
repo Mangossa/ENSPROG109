4703d63633643b7f1b29a9c88d3d0adea4be2ebe
Mangossa <130641365+Mangossa@users.noreply.github.com>
2023-05-13 23:40:36 -0700

    Update diamond.js

--- diamond.js
+++ diamond.js
@@ -56,3 +56,52 @@ rLine +="</p>";
 
 document.getElementById("downRight").innerHTML = rLine;
 }
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+  var rLine = "";
+  for (i = pHeight; i > 0; i--) {
+    rLine += "<p>";
+    //Add spaces to the left side
+    for (j = 0; j < pHeight - i; j++) {
+      rLine += "&nbsp;";
+    }
+    //Create each line on the Rhombus
+    for (j = 0; j < i; j++) {
+      //Is the position even or odd so we change the color
+      if (j % 2) {
+        //even
+        rLine +=
+          "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      } else {
+        //odd
+        rLine +=
+          "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+      }
+    }
+    rLine += "</p>";
+  }
+  document.getElementById("upLeft").innerHTML = rLine;
+}
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+  var rLine = "";
+  for (i = pHeight; i > 0; i--) {
+    rLine += "<p>";
+    // Create each line on the Rhombus
+    for (j = 0; j < pHeight - i; j++) {
+      // Add spaces for indentation
+      rLine += "&nbsp;";
+    }
+    for (j = 0; j < i; j++) {
+      // Is the position even or odd so we change the color
+      if (j % 2) {
+        // even
+        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      } else {
+        // odd
+        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+      }
+    }
+    rLine += "</p>";
+    // console.log(rLine);
+  }
+  document.getElementById("downLeft").innerHTML = rLine;
+}
