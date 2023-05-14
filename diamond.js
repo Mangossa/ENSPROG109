ddf4dfd9993135b712a93bed4e18879aa2279963
Mangossa <130641365+Mangossa@users.noreply.github.com>
2023-05-14 01:59:07 -0700

    Update diamond.js

--- diamond.js
+++ diamond.js
@@ -1,3 +1,8 @@
+// var rHeight =5;
+// var colorEven = "orange";
+// var colorOdd = "black";
+// var symbol ="*";
+
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
 upRight(pHeight, pColorEven, pColorOdd, pSymbol);
 downRight(pHeight, pColorEven, pColorOdd, pSymbol);
@@ -50,4 +55,43 @@ rLine +="</p>";
 }
 
 document.getElementById("downRight").innerHTML = rLine;
+}
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+  var rLine = "";
+  for (i = pHeight; i >= 0; i--) {
+    rLine += "<p>";
+    //Create each line on the Rhombus
+    for (j = i; j < pHeight; j++) {
+      //Is the position even or odd so we change the color
+      if (j % 2)
+        //even
+        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      else
+        //odd
+        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+    }
+    rLine += "</p>";
+  }
+
+  document.getElementById("upLeft").innerHTML = rLine;
+}
+
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+  var rLine = "";
+  for (i = 0; i <= pHeight; i++) {
+    rLine += "<p>";
+    //Create each line on the Rhombus
+    for (j = i; j < pHeight; j++) {
+      //Is the position even or odd so we change the color
+      if (j % 2)
+        //even
+        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      else
+        //odd
+        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+    }
+    rLine += "</p>";
+  }
+
+  document.getElementById("downLeft").innerHTML = rLine;
 }
