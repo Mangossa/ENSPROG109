f0a1e2dbf352b5918a2d0ea00e73d41231e8c501
Mangossa <130641365+Mangossa@users.noreply.github.com>
2023-05-14 01:28:35 -0700

    Update diamond.js

--- diamond.js
+++ diamond.js
@@ -3,10 +3,15 @@
 // var colorOdd = "black";
 // var symbol ="*";
 
+	
+// var rHeight =5;
+// var colorEven = "orange";
+// var colorOdd = "black";
+// var symbol ="*";
+
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
-upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
-
-
+upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+downRight(pHeight, pColorEven, pColorOdd, pSymbol);
 
 }
 
@@ -56,53 +61,4 @@ rLine +="</p>";
 }
 
 document.getElementById("downRight").innerHTML = rLine;
-}
-function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
-  var rLine = "";
-  for (i = pHeight; i > 0; i--) {
-    rLine += "<p>";
-    //Add spaces to the left side
-    for (j = 0; j < pHeight - i; j++) {
-      rLine += "&nbsp;";
-    }
-    //Create each line on the Rhombus
-    for (j = 0; j < i; j++) {
-      //Is the position even or odd so we change the color
-      if (j % 2) {
-        //even
-        rLine +=
-          "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-      } else {
-        //odd
-        rLine +=
-          "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-      }
-    }
-    rLine += "</p>";
-  }
-  document.getElementById("upLeft").innerHTML = rLine;
-}
-function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
-  var rLine = "";
-  for (i = pHeight; i > 0; i--) {
-    rLine += "<p>";
-    // Create each line on the Rhombus
-    for (j = 0; j < pHeight - i; j++) {
-      // Add spaces for indentation
-      rLine += "&nbsp;";
-    }
-    for (j = 0; j < i; j++) {
-      // Is the position even or odd so we change the color
-      if (j % 2) {
-        // even
-        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-      } else {
-        // odd
-        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-      }
-    }
-    rLine += "</p>";
-    // console.log(rLine);
-  }
-  document.getElementById("downLeft").innerHTML = rLine;
 }
